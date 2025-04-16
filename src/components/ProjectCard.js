140521ffd8c1dbc54af333cf8d4878803aafad28
Dongsusin <auroratime020715@gmail.com>
2025-04-16 14:46:24 +0900

    change project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,17 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, imgUrl, url }) => {
+export const ProjectCard = ({ title, imgUrl, url, githuburl, useskill }) => {
   return (
     <Col size={12} xl={4} lg={6} className="proj-box">
       <div className="proj-imgbx">
         <img src={imgUrl} alt="imgurl" />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <a href={url}>사이트</a>
+          <h2>사용기술:{useskill}</h2>
+          <div className="urlbox">
+            <a href={url}>사이트</a>
+            <a href={githuburl}>github</a>
+          </div>
         </div>
       </div>
     </Col>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,6 +13,7 @@ import soloimg1 from "../assets/img/weather.png";
 import soloimg2 from "../assets/img/change.png";
 import soloimg3 from "../assets/img/pokedex.png";
 import soloimg4 from "../assets/img/todolist.png";
+import soloimg5 from "../assets/img/protfolio.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
@@ -22,26 +23,36 @@ export const Projects = () => {
       title: "Pokemon",
       imgUrl: siteimg1,
       url: "https://dongsusin.github.io/01-CITE/01-pokomon/pocketmon.html",
+      githuburl: "https://github.com/Dongsusin/01-CITE/tree/main/01-pokomon",
+      useskill: "Html,Css,js",
     },
     {
       title: "Neowiz",
       imgUrl: siteimg2,
       url: "https://dongsusin.github.io/01-CITE/02-neowiz/neowiz.html",
+      githuburl: "https://github.com/Dongsusin/01-CITE/tree/main/02-neowiz",
+      useskill: "Html,Css,js",
     },
     {
       title: "Kakao",
       imgUrl: siteimg3,
       url: "https://dongsusin.github.io/01-CITE/03.kakao/kakao.html",
+      githuburl: "https://github.com/Dongsusin/01-CITE/tree/main/03.kakao",
+      useskill: "Html,Css,js",
     },
     {
       title: "Kaist",
       imgUrl: siteimg4,
       url: "https://dongsusin.github.io/01-CITE/04-kaist/kaist.html",
+      githuburl: "https://github.com/Dongsusin/01-CITE/tree/main/04-kaist",
+      useskill: "Html,Css,js",
     },
     {
       title: "Nexon",
       imgUrl: siteimg5,
       url: "https://dongsusin.github.io/01-CITE/05-nexon/nexon.html",
+      githuburl: "https://github.com/Dongsusin/01-CITE/tree/main/05-nexon",
+      useskill: "Html,Css,js",
     },
   ];
   const secondprojects = [
@@ -49,21 +60,29 @@ export const Projects = () => {
       title: "반응속도 테스트",
       imgUrl: gameimg1,
       url: "https://dongsusin.github.io/02-GAME/01-seped/speed.html",
+      githuburl: "https://github.com/Dongsusin/02-GAME/tree/main/01-seped",
+      useskill: "Html,Css,js",
     },
     {
       title: "지뢰찾기",
       imgUrl: gameimg2,
       url: "https://dongsusin.github.io/02-GAME/02-search/search.html",
+      githuburl: "https://github.com/Dongsusin/02-GAME/tree/main/02-search",
+      useskill: "Html,Css,js",
     },
     {
       title: "테트리스",
       imgUrl: gameimg3,
       url: "https://dongsusin.github.io/02-GAME/03-tetris/tetris.html",
+      githuburl: "https://github.com/Dongsusin/02-GAME/tree/main/03-tetris",
+      useskill: "Html,Css,js",
     },
     {
       title: "2048",
       imgUrl: gameimg4,
       url: "https://dongsusin.github.io/02-GAME/04-2048/2048.html",
+      githuburl: "https://github.com/Dongsusin/02-GAME/tree/main/04-2048",
+      useskill: "Html,Css,js",
     },
   ];
   const thirdprojects = [
@@ -71,21 +90,37 @@ export const Projects = () => {
       title: "날씨GPS",
       imgUrl: soloimg1,
       url: "https://dongsusin.github.io/03-ANI-API/02-weather/weather.html",
+      githuburl: "https://github.com/Dongsusin/03-ANI-API/tree/main/02-weather",
+      useskill: "Html,Css,Js,API",
     },
     {
       title: "환률계산기",
       imgUrl: soloimg2,
       url: "https://dongsusin.github.io/03-ANI-API/03-adjustment/exchange.html",
+      githuburl:
+        "https://github.com/Dongsusin/03-ANI-API/tree/main/03-adjustment",
+      useskill: "Html,Css,Js,API",
     },
     {
       title: "포켓몬API도감",
       imgUrl: soloimg3,
       url: "https://brilliant-alfajores-bbb341.netlify.app/",
+      githuburl: "https://github.com/Dongsusin/01-pokemon-pokedex",
+      useskill: "React,API,Css",
     },
     {
       title: "To-Do-List",
       imgUrl: soloimg4,
       url: "https://shimmering-duckanoo-86e550.netlify.app/",
+      githuburl: "https://github.com/Dongsusin/02-todolist",
+      useskill: "React,API,Css",
+    },
+    {
+      title: "포트폴리오",
+      imgUrl: soloimg5,
+      url: "https://luxury-vacherin-425521.netlify.app/",
+      githuburl: "https://github.com/Dongsusin/03-portfolio",
+      useskill: "React,API,Css",
     },
   ];
 
@@ -126,7 +161,6 @@ export const Projects = () => {
                     >
                       <Tab.Pane eventKey="first">
                         <Row className="row-skill">
-                          <p className="project-title">사용 스킬:Html,Css,Js</p>
                           {firstprojects.map((project, index) => {
                             return <ProjectCard key={index} {...project} />;
                           })}
@@ -134,7 +168,6 @@ export const Projects = () => {
                       </Tab.Pane>
                       <Tab.Pane eventKey="second">
                         <Row className="row-skill">
-                          <p className="project-title">사용 스킬:Html,Css,Js</p>
                           {secondprojects.map((project, index) => {
                             return <ProjectCard key={index} {...project} />;
                           })}
@@ -142,7 +175,6 @@ export const Projects = () => {
                       </Tab.Pane>
                       <Tab.Pane eventKey="third">
                         <Row className="row-skill">
-                          <p className="project-title">사용 스킬:React,API</p>
                           {thirdprojects.map((project, index) => {
                             return <ProjectCard key={index} {...project} />;
                           })}
