4434daa3054b690f371388c9173b6a13bd01f586
Dongsusin <auroratime020715@gmail.com>
2025-05-11 20:03:52 +0900

    프로젝트 전체 수정

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,8 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import siteimg1 from "../assets/img/pokemon.png";
-import siteimg2 from "../assets/img/neo.png";
-import siteimg3 from "../assets/img/kakao.png";
-import siteimg4 from "../assets/img/seven.png";
+import mainimg1 from "../assets/img/seven.png";
 import soloimg1 from "../assets/img/weather.png";
+import soloimg2 from "../assets/img/musicplayer-preview.png";
 import soloimg3 from "../assets/img/pokedex.png";
 import soloimg4 from "../assets/img/todolist.png";
 import soloimg5 from "../assets/img/protfolio.png";
@@ -15,32 +13,11 @@ export const Projects = () => {
   //첫번째 프로젝트 배열
   const firstprojects = [
     {
-      title: "Pokemon",
-      imgUrl: siteimg1,
-      url: "https://dongsusin.github.io/pokemon/",
-      githuburl: "https://github.com/Dongsusin/pokemon",
-    },
-    {
-      title: "Neowiz",
-      imgUrl: siteimg2,
-      url: "https://dongsusin.github.io/neowiz/",
-      githuburl: "https://github.com/Dongsusin/neowiz",
-    },
-    {
-      title: "Kakao",
-      imgUrl: siteimg3,
-      url: "https://dongsusin.github.io/kakao/",
-      githuburl: "https://github.com/Dongsusin/kakao",
-    },
-    {
       title: "밀리의 서재",
       imgUrl: mile,
       url: "https://9rodigital-willie.netlify.app/",
       githuburl: "https://github.com/FRONTENDBOOTCAMP-13th/9RoDigital",
     },
-  ];
-  //두번째 프로젝트 배열
-  const secondprojects = [
     {
       title: "날씨GPS",
       imgUrl: soloimg1,
@@ -48,16 +25,16 @@ export const Projects = () => {
       githuburl: "https://github.com/Dongsusin/weather",
     },
     {
+      title: "뮤직플레이어",
+      imgUrl: soloimg2,
+      url: "https://dongsusin.github.io/music-player/",
+      githuburl: "https://github.com/Dongsusin/music-player",
+    },
+    {
       title: "포켓몬API도감",
       imgUrl: soloimg3,
       url: "https://dongsusin.github.io/pokemon-pokedex/",
       githuburl: "https://github.com/Dongsusin/pokemon-pokedex",
-    },
-    {
-      title: "세븐나이츠",
-      imgUrl: siteimg4,
-      url: "https://stalwart-granita-3bb4c2.netlify.app/",
-      githuburl: "https://dongsusin.github.io/seven/",
     },
     {
       title: "TO-DO-LIST",
@@ -70,6 +47,15 @@ export const Projects = () => {
       imgUrl: soloimg5,
       url: "https://dongsusin.github.io/portfolio/",
       githuburl: "https://github.com/Dongsusin/portfolio",
+    },
+  ];
+  //두번째 프로젝트 배열
+  const secondprojects = [
+    {
+      title: "세븐나이츠",
+      imgUrl: mainimg1,
+      url: "https://stalwart-granita-3bb4c2.netlify.app/",
+      githuburl: "https://dongsusin.github.io/seven/",
     },
   ];
   return (
@@ -87,10 +73,10 @@ export const Projects = () => {
                     id="pills-tab"
                   >
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Site</Nav.Link>
+                      <Nav.Link eventKey="first">toi project</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">toi project</Nav.Link>
+                      <Nav.Link eventKey="second">main project</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp">
